0041abce53bae9d5e66ee77b34eb2bc569e6e684
Carver Harrison <pcarverb@hotmail.com>
2016-10-12 20:38:33 -0700

    Syntax

--- js/kernel.js
+++ js/kernel.js
@@ -4,10 +4,10 @@
 */
 
 // Variable definitions
-const SHIFTOS_VERSION
+const SHIFTOS_VERSION;
 
 // Kernel init
 function kernel_init() {
-  SHIFTOS_VERSION = "1.0"
-  console.log("Started kernel")
+  SHIFTOS_VERSION = "1.0";
+  console.log("Started kernel");
 }
