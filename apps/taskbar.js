6ba632ad23580089ded0c22b4199b1bec0740b71
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 14:29:29 -0700

    Create taskbar.js

---
+++ apps/taskbar.js
@@ -0,0 +1,7 @@
+apps.taskbar = {
+  main: function() {
+    $("#ui").append("<div class='ui-taskbar'><div id='utm' class='ui-taskbar-menu'></div><div id='utt' class='ui-taskbar-tasks'></div><div id='utr' class='ui-taskbar-tray'></div></div>");
+  }
+}
+
+apps.taskbar.main()
