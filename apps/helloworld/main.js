7d74a9a58b3a45e9c6555ae64e58b28f9d42f405
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 13:13:03 -0700

    Create main.js

---
+++ apps/helloworld/main.js
@@ -0,0 +1,9 @@
+var apps.helloworld = {
+  main: function() {
+    // ShiftOS bootstrap for graphical apps
+    $("#ui").append("<div class='ui-app' id='app-helloworld' title='Hello World'><h1>Hello, World!</h1></div>");
+    $("#app-helloworld").dialog();
+  }
+}
+
+apps.helloworld.main();
