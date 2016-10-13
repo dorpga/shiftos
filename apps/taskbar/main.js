f213f0559f27dd3cf8829222f7c14752b4fc4d39
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 14:42:17 -0700

    Update main.js

--- apps/taskbar/main.js
+++ apps/taskbar/main.js
@@ -1,6 +1,6 @@
 apps.taskbar = {
   main: function() {
-    $("#ui").append("<div class='ui-taskbar'><div id='utm' class='ui-taskbar-menu'><div id='utmb' class='ui-taskbar-menu-button'></div><div id='utmm' class='ui-taskbar-menu-list'></div></div><div id='utt' class='ui-taskbar-tasks'></div><div id='utr' class='ui-taskbar-tray'></div></div>");
+    $("#ui").append("<div class='ui-taskbar'><div id='utm' class='ui-taskbar-menu'><div id='utmb' class='ui-taskbar-menu-button'>Menu</div><div id='utmm' class='ui-taskbar-menu-list'></div></div><div id='utt' class='ui-taskbar-tasks'></div><div id='utr' class='ui-taskbar-tray'></div></div>");
   }
 }
 
