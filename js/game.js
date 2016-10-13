bb9119c20b3dad9fbd28ed06bd2323beecc8ae5f
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 10:11:49 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -9,7 +9,7 @@ var game = {
 
 // Game init
 function game_init() {
-  game.firstRun = !Cookies.get('started');  // Check first run
-  if (game.firstRun) console.log("Game has not been started before");  // Print out if the game has been run before
-  console.log("Started game");  // Finish game init
+  game.firstRun = !Cookies.get('started'); // Check first run
+  if (game.firstRun) console.log("Game has not been started before"); // Print out if the game has been run before
+  console.log("Started game"); // Finish game init
 }
