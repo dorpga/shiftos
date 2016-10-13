e742add79cb89d9ef1234faa10e41e9e4724ae85
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 11:06:00 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -9,7 +9,14 @@ var UI = {
   },
   openCliApp: function(app) {
   },
-  openDialog: function(title, message, buttons, image) {
+  openDialog: function(title, message, icon, type) {
+    var buttons;
+    switch (type) {
+      case "message":
+        buttons = "<button class='ui-dialog-button-submit'>OK</button>";
+        break;
+    }
+    $("#ui-display").append("<div class='ui-dialog' " + title + "'><div class='ui-dialog-icon'><img src='images/dialog/" + icon + "' height='64' width='64' alt='" + icon + "' />" + "</div><div class='ui-dialog-message'>" + message + "</div><div class='ui-dialog-buttons'>" + buttons + "</div></div>");
   },
   closeApp: function(app) {
   },
