290ee19fe846abe429edb58844675949f406a1ac
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 10:09:37 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -7,7 +7,9 @@ var game = {
   firstrun: true
 }
 
+// Game init
 function game_init() {
-  game.firstRun = !Cookies.get('started');
-  console.log("Started game")
+  game.firstRun = !Cookies.get('started');  // Check first run
+  if (game.firstRun) console.log("Game has not been started before");  // Print out if the game has been run before
+  console.log("Started game");  // Finish game init
 }
