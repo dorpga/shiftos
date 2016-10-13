99994d99dd3a9e2d6231e26f6c579c98ac8208d1
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 12:09:44 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -6,11 +6,11 @@
 var game = {
   firstrun: true,
   intro: function() {
-    Cookies.set("started", true, {path: 'game'});
+    Cookies.set("started", true, {path: ''});
   },
   debug: {
     resetFirstRun: function() {
-      Cookies.remove("started", {path: 'game'});
+      Cookies.remove("started", {path: ''});
       console.log("Finished reseting first run");
     }
   }
@@ -18,7 +18,7 @@ var game = {
 
 // Game init
 function game_init() {
-  game.firstRun = !Cookies.get("started", {path: 'game'}); // Check first run
+  game.firstRun = !Cookies.get("started", {path: ''}); // Check first run
   if (game.firstRun) {
     console.log("Game has not been started before"); // Print out if the game has been run before
     game.intro();
