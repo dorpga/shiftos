985d43ec2abd80b0a1504a0787295029a708e54c
Carver Harrison <pcarverb@hotmail.com>
2016-10-12 20:32:57 -0700

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+/*
+ * ShiftOS Init
+ * Copyright 2016 Carver Harrison
+*/
+
+kernel_init() // Init the kernel
+console.log("ShiftOS Version " + SHIFTOS_VERSION") // Print out the version
+console.log("Copyright 2016 Carver Harrison") // Print out copyright notice
