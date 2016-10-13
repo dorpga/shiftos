df5c832725fd58ddc11c465f5f21c679ea51612c
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 16:31:22 -0700

    Update main.js

--- apps/taskbar/main.js
+++ apps/taskbar/main.js
@@ -1,7 +1,22 @@
 apps.taskbar = {
   main: function() {
-    $("#ui").append("<div class='ui-taskbar'><div id='utm' class='ui-taskbar-menu'><div id='utmb' class='ui-taskbar-menu-button'>Menu</div><div id='utmm' class='ui-taskbar-menu-list'></div></div><div id='utt' class='ui-taskbar-tasks'></div><div id='utr' class='ui-taskbar-tray'></div></div>");
+    $("#ui").append("\
+                    <div class='ui-taskbar'>\
+                      <div id='utm' class='ui-taskbar-menu'>\
+                        <div id='utmb' class='ui-taskbar-menu-button'>Menu</div>\
+                        <div id='utmm' class='ui-taskbar-menu-list'></div>\
+                      </div>\
+                      <div id='utt' class='ui-taskbar-tasks'></div>\
+                      <div id='utr' class='ui-taskbar-tray'></div>\
+                      </div>\
+    ");
+    
+    $("#utmm").menu();
+    $("#utmm").hide();
+    $("#utmb").click(function () {
+      $("#utmm").toggle();
+    });
   }
-}
+};
 
 apps.taskbar.main()
