dad3bddeb2729a1d70fddf3db24bb75868c68ee8
Carver Harrison <pcarverb@hotmail.com>
2016-10-12 21:38:55 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -4,4 +4,4 @@
 */
 
 kernel_init(); // Init the kernel
-console.log("ShiftOS Version " + SHIFTOS_VERSION + "\nCopyright 2016 Carver Harrison"); // Print out copyright notice
+console.log("ShiftOS " + SHIFTOS_VERSION + " Copyright 2016 Carver Harrison"); // Print out copyright notice
