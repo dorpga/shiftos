e32399cb56fa3ab6a1b9ff0650367a587348a24b
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 10:10:03 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@
  * Copyright 2016 Carver Harrison
 */
 
-kernel_init(); // Init the kernel
-filesystem_init(); // Init the filesystem
-game_init(); // Init the game
-console.log("ShiftOS " + SHIFTOS_VERSION); // Print out version
+kernel_init();  // Init the kernel
+filesystem_init();  // Init the filesystem
+game_init();  // Init the game
+console.log("ShiftOS " + SHIFTOS_VERSION);  // Print out version
