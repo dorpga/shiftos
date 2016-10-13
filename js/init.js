4d0277b38e467fd09dc8a3a6e27e681adf3b2434
Carver Harrison <pcarverb@hotmail.com>
2016-10-13 11:09:08 -0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,4 +8,4 @@ filesystem_init(); // Init the filesystem
 game_init(); // Init the game
 ui_init(); // Init the ui
 console.log("ShiftOS " + SHIFTOS_VERSION); // Print out version
-UI.createDialog("ShiftOS", "ShiftOS Started", "finished-init.png", "message"); // Create welcome dialog
+UI.openDialog("ShiftOS", "ShiftOS Started", "finished-init.png", "message"); // Create welcome dialog
