4364ef49e583040e86f03b695f84500d3d83f4dc
Carver Harrison <pcarverb@hotmail.com>
2016-10-14 12:35:22 -0700

    Update main.js

--- apps/shiftorium/main.js
+++ apps/shiftorium/main.js
@@ -3,28 +3,44 @@ apps.shiftorium = {
     var did = kernel.random.base26(4);
     $("#ui").append(`
       <div class='ui-shiftorium' id='app-` + did + `'>
-        <div class='ui-shiftorium-title'>
-          <h1>Shiftorium</h1>
-        </div>
-        <div class='ui-shiftorium-upgrades'>
-          <ul class='ui-shiftorium-upgrades-list'>
-          </ul>
-        </div>
-        <div class='ui-shiftorium-info'>
-            <img 
-              src='/images/shiftorium/welcome.png'
-              height='256'
-              width='256'
-              alt='Shiftorium Upgrade Image' 
-              class='ui-shiftorium-image' 
-            />
-            <h2 class='ui-shiftorium-info-title'>Welcome to the Shiftorium!</h3>
-            <p class='ui-shiftorium-info-text'>Select a upgrade for more info.</p>
-        </div>
-        <div class='ui-shiftorium-buttons'>
-          <button class='btn ui-shiftorium-button ui-shiftorium-button-buy'>Buy</button>
-          <button class='btn ui-shiftorium-button ui-shiftorium-button-exit'>Exit</button>
-        </div>
+        <table>
+          <tr>
+            <td colspan="2">
+              <div class='ui-shiftorium-title'>
+                <h1>Shiftorium</h1>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class='ui-shiftorium-upgrades'>
+                <ul class='ui-shiftorium-upgrades-list'>
+                </ul>
+              </div>
+            </td>
+            <td>
+              <div class='ui-shiftorium-info'>
+                  <img 
+                    src='/images/shiftorium/welcome.png'
+                    height='256'
+                    width='256'
+                    alt='Shiftorium Upgrade Image' 
+                    class='ui-shiftorium-image' 
+                  />
+                  <h2 class='ui-shiftorium-info-title'>Welcome to the Shiftorium!</h3>
+                  <p class='ui-shiftorium-info-text'>Select a upgrade for more info.</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class='ui-shiftorium-buttons'>
+                <button class='btn ui-shiftorium-button ui-shiftorium-button-buy'>Buy</button>
+                <button class='btn ui-shiftorium-button ui-shiftorium-button-exit'>Exit</button>
+              </div>
+            </td>
+          </tr>
+        </table>
       </div>
     `);
     $("#app-" + did).dialog();
