e88403adc0d81fbe848668daaaa7abc3764ef5d2
Carver Harrison <pcarverb@hotmail.com>
2016-10-14 10:10:28 -0700

    Update main.js

--- apps/terminal/main.js
+++ apps/terminal/main.js
@@ -6,8 +6,29 @@
 apps.terminal = {
   tty: 0,
   main: function() {
-    $("#ui").append("<div class='ui-terminal-container' id='tty" + apps.terminal.tty + "'><pre class='ui-terminal'></pre><div class='ui-tty-input-container'><input class='ui-tty-input' id='ttyin-" + apps.terminal.tty + "><button class='ui-tty-submit-button' id='ttysubmit-" + apps.terminal.tty + "'>OK</button></div></div>" );
+    $("#ui").append("<div class='ui-terminal' id='tty" + apps.terminal.tty + "'></div>" );
+    function($, undefined) {
+    $('#tty' + apps.terminal.tty).terminal(function(command, term) {
+        var cmd = command.split(' ');
+        switch (cmd[0]) {
+          case "ver":
+            term.echo("ShiftOS Version " + SHIFTOS_VERSION);
+            break;
+          case "help":
+            term.echo("help - Command list");
+            term.echo("ver - Version");
+            break;
+          default:
+            term.echo("Unknown Command: " + cmd[0]);
+        }
+    }, {
+        greetings: 'Javascript Interpreter',
+        name: 'js_demo',
+        height: 200,
+        prompt: 'js> '
+    });
     $("#tty" + apps.terminal.tty).dialog();
+    apps.terminal.tty++;
   }
 }
 
