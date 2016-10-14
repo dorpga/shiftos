7f8e54521d27d2c448b3ad2fcd0767a148407761
Carver Harrison <pcarverb@hotmail.com>
2016-10-14 10:03:21 -0700

    Update main.js

--- apps/terminal/main.js
+++ apps/terminal/main.js
@@ -6,7 +6,7 @@
 apps.terminal = {
   tty: 0,
   main: function() {
-    $("#ui").append("<div class='ui-terminal-container' id='tty" + apps.terminal.tty + "'><pre class='ui-terminal'></pre><div class='ui-tty-input-container'><input class='ui-tty-input' id='ttyin-" + apps.terminal.tty + "><button class='ui-tty-submit-button' id='ttysubmit-" + apps.terminal.tty + "'></button></div></div>" );
+    $("#ui").append("<div class='ui-terminal-container' id='tty" + apps.terminal.tty + "'><pre class='ui-terminal'></pre><div class='ui-tty-input-container'><input class='ui-tty-input' id='ttyin-" + apps.terminal.tty + "><button class='ui-tty-submit-button' id='ttysubmit-" + apps.terminal.tty + "'>OK</button></div></div>" );
     $("#tty" + apps.terminal.tty).dialog();
   }
 }
