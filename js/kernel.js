a738d0d1cf6c612e754043c8e304f624c34501e8
Carver Harrison <pcarverb@hotmail.com>
2016-10-14 12:13:55 -0700

    Update kernel.js

--- js/kernel.js
+++ js/kernel.js
@@ -5,6 +5,17 @@
 
 // Object definitions
 var apps = {}; // App Containers
+var kernel = {
+  random: {
+    base26: function(length) {
+      var text = "";
+      var possible = "abcdefghijklmnopqrstuvwxyz";
+      for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      return text;
+    }
+  }
+};
 
 // Variable definitions
 var SHIFTOS_VERSION = "0.4.2"; // ShiftOS Version
