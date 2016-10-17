f919c9ea4428c020c9d0dd1ba4ca872c410ed0a0
Carver Harrison <pcarverb@hotmail.com>
2016-10-17 12:18:32 -0700

    Update main.js

--- apps/shiftorium/main.js
+++ apps/shiftorium/main.js
@@ -44,7 +44,7 @@ apps.shiftorium.main();
 for (i = 0; i < Object.getOwnPropertyNames(allUpgrades).length; i++) {
    $('.ui-shiftorium-upgrades-list').append(`
     <li>
-      <a href="javascript:apps.shiftorium.showUpgrade(` + allUpgrades[Object.getOwnPropertyNames(allUpgrades)[i]].constructor.name + `)">` + allUpgrades[Object.getOwnPropertyNames(allUpgrades)[i]].name + `</a>
+      <a href="javascript:apps.shiftorium.showUpgrade(` + allUpgrades[Object.getOwnPropertyNames(allUpgrades)[i]].iname + `)">` + allUpgrades[Object.getOwnPropertyNames(allUpgrades)[i]].name + `</a>
     </li>`
    );
    console.log("Added " + allUpgrades[Object.getOwnPropertyNames(allUpgrades)[i]].name + " to shiftorium list");
