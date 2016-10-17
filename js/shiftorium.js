9350872b7ac09389d208cc8b5338b5209f2dbd7d
Carver Harrison <pcarverb@hotmail.com>
2016-10-17 09:43:03 -0700

    Create shiftorium.js

---
+++ js/shiftorium.js
@@ -0,0 +1,16 @@
+/*
+ * ShiftOS Shiftorium
+ * Copyright 2016 Carver Harrison
+*/
+
+// Enable an upgrade
+function enableUpgrade(name) {
+}
+
+// Disable an upgrade
+function disableUpgrade(name) {
+}
+
+// Check an upgrade's status
+function checkUpgrade(name) {
+}
