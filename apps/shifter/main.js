650f4445c423a0c4fec881e05a16478170546ca6
Carver Harrison <pcarverb@hotmail.com>
2016-10-18 11:43:49 -0700

    Create main.js

---
+++ apps/shifter/main.js
@@ -0,0 +1,34 @@
+apps.shifter = {
+  main: function() {
+    var myId = kernel.random.base26(4);
+    $(".ui").append(`
+      <div id='app-` + myId + `'>
+        <table>
+          <thead>
+            <tr>
+              <td>Shiftlets</td>
+              <td>Settings</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <ul id='ss-` + myId + `' class='shifter-shiftlets'>
+                </ul>
+              </td>
+              <td>
+                <ul id='st-` + myId + `' class='shifter-settings'>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <span>
+              <button onlick='apps.shifter.apply()'>Apply</button>
+              <button onlick='apps.shifter.reset()'>Reset</button>
+            </span>
+          </tfoot>
+        </table>
+      </div>
+    `);
+  }
+}
